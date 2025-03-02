db42f195c5cd1cd0eb15fb3303b6d7d2ae3dedaa
fulpopy <akash16799@gmail.com>
2025-03-02 10:39:54 +0530

    feat: add .env file for API URL configuration and update api.js to use environment variable

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+const url = process.env.REACT_APP_API_URL || "http://localhost:5000";
 export const loginUser = async (user) => {
   try {
     const res = await axios.post(`${url}/api/users`, user);
